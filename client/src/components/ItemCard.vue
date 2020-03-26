984d1f59b6bb58474bdc8e82f80f14df702ebcc3
Evan <evjava@gmail.com>
2020-03-26 10:10:16 -0400

    init commit

---
+++ client/src/components/ItemCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-card :img-src="itemPictureUrl" :title="itemName" img-top>
+      <b-card-text>
+        <p>{{ itemDescription }}</p>
+        <h5>${{ itemPrice }}</h5>
+      </b-card-text>
+    </b-card>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemCard",
+  data(){
+    return{
+      
+    }
+  },
+  props: {
+    itemName: String,
+    itemPrice: Number,
+    itemId: String,
+    itemPictureUrl: String,
+    itemDescription: String
+  }
+
+}
+</script>
+
+<style>
+
+</style>
